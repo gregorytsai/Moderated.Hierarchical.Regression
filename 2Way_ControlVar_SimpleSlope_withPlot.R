2dab853e9cef2f7f00bcc7c912e9ededb1465164
gregorytsai <gregorytsai@hotmail.com>
2016-02-29 01:16:24 +0800

    Edit description

--- 2Way_ControlVar_SimpleSlope_withPlot.R
+++ 2Way_ControlVar_SimpleSlope_withPlot.R
@@ -1,5 +1,6 @@
 ##################################################################################
-##2016.2.26
+##2016.2.28
+##Can be very slow if your data is large, use with caution
 ##Author:GregTsai   Email:gregorytsai@gmail.com
 ##Graduate student at National Taiwan University, department of Psychology
 ##Developed and tested on R Ver.3.2.3 Windows
